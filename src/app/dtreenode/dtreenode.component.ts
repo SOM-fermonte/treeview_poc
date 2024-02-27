f2575f7cbc8227825dd3f196a095627ccbe23153
SOM-fermonte <SOM-fermonte@users.noreply.github.com>
2024-02-27 08:27:44 -0500

    new app component for nodes

---
+++ src/app/dtreenode/dtreenode.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TreeNode, TreeModel, TREE_ACTIONS, KEYS, IActionMapping, ITreeOptions } from '@circlon/angular-tree-component';
+
+@Component({
+  selector: 'app-dtreenode',
+  styleUrls: ['./dtreenode.component.css'],
+  templateUrl: './dtreenode.component.html'
+})
+
+export class DTreeNodeComponent extends TreeNode {
+  
+}
