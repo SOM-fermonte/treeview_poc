a83331c005a5f4bfded7496d64a59c02b838f7fe
SOM-fermonte <SOM-fermonte@users.noreply.github.com>
2024-03-02 21:08:16 -0500

    adding constructor and OnInit to DTreeNodeComponent

--- src/app/dtreenode/dtreenode.component.ts
+++ src/app/dtreenode/dtreenode.component.ts
@@ -7,7 +7,7 @@ import { TreeNode, TreeModel, TREE_ACTIONS, KEYS, IActionMapping, ITreeOptions }
   templateUrl: './dtreenode.component.html'
 })
 
-export class DTreeNodeComponent extends TreeNode {
+export class DTreeNodeComponent extends TreeNode implements OnInit {
   qCode: string;
   qType: number;
   qRequired: boolean;
@@ -16,4 +16,12 @@ export class DTreeNodeComponent extends TreeNode {
   pass: boolean;
   messageText: string;
   children: DTreeNodeComponent[];
+
+  constructor() {
+    super();
+  }
+
+  ngOnInit(): void {
+
+  }
 }
