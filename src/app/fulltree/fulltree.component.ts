311adcab25c9d59c0769a350d0e0408129a347b8
SOM-fermonte <SOM-fermonte@users.noreply.github.com>
2024-03-03 23:05:03 -0500

    test to add nodes

--- src/app/fulltree/fulltree.component.ts
+++ src/app/fulltree/fulltree.component.ts
@@ -34,17 +34,6 @@ const actionMapping: IActionMapping = {
 
 @Component({
   selector: 'app-fulltree',
-  styles: [
-    `button: {
-        line - height: 24px;
-        box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.5);
-        border: none;
-        border-radius: 2px;
-        background: #A3D9F5;
-        cursor: pointer;
-        margin: 0 3px;
-      }`
-  ],
   templateUrl: './fulltree.component.html'
 })
 
@@ -126,7 +115,7 @@ export class FullTreeComponent implements OnInit {
   addNode(tree: any) {
     this.nodes[0].children.push({
 
-      name: 'a new child'
+      uuid: 'a new child'
     });
     tree.treeModel.update();
   }
